8a3f8382ddb028b16cf62884f816dc883e61cbb9
dbueno <danielbueno76@gmail.com>
2021-11-03 12:16:32 +0100

    environment common

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,4 @@
+export const commonEnvironment = {
+    successLoginRoute: '/listaEmpleados',
+    empleadosServiceUrl: 'http://localhost:5555/empleados/',
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+import { commonEnvironment } from './environment.common';
 export const environment = {
-  production: true
+  production: true,
+  common: commonEnvironment
 };
